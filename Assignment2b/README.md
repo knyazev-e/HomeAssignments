602ce7106480a8ce74ceae9fe106e0b5118c7707
vboxuser <vboxuser@Uninstall.myguest.virtualbox.org>
2024-12-19 19:39:24 +0000

    final changes

---
+++ Assignment2b/README.md
@@ -0,0 +1,11 @@
+# Assignment 2b
+## Author
+Князева Ева Андреевна, группа 24.Б81-мм
+## Contacts
+st128104@student.spbu.ru
+## Description
+A calculator that does 4 operatons and uses the reversed polish notation.
+## Build
+make
+## Run
+./run
---
+++ Assignment2b/calc.cpp
@@ -0,0 +1,52 @@
+/* Kniazeva Eva Andreevna
+st128104@student.spbu.ru */
+
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+
+int main() {
+    std::string input_line;
+    std::cout << "Input the expression in reversed polish notation: " << std::endl;
+    std::getline(std::cin, input_line);
+
+    int size = (std::count(input_line.begin(), input_line.end(), ' ')/2) + 1;
+    float *numbers = new float[size];
+
+    std::istringstream stream(input_line);
+    std::string word;
+
+    int index = 0;
+    while(stream >> word) {
+        if (word == "-") {
+            numbers[index - 2] = numbers[index - 2] - numbers[index - 1];
+            numbers[index] = 0;
+            index--;
+        }
+        else if (word == "+") {
+            numbers[index - 2] = numbers[index - 2] + numbers[index - 1];
+            numbers[index] = 0;
+            index--;
+        }
+        else if (word == "/") {
+            numbers[index - 2] = numbers[index - 2] / numbers[index - 1];
+            numbers[index] = 0;
+            index--;
+        }
+        else if (word == "*") {
+            numbers[index - 2] = numbers[index - 2] * numbers[index - 1];
+            numbers[index] = 0;
+            index--;
+        }
+        else {
+            numbers[index] = std::stof(word);
+            index++;
+        }
+    }
+    std::cout << numbers[0] << std::endl;
+
+    delete[]
+}
